f718da8ad062aa3bc54a97526aefeb83b4debfda
Rajmalbasha <68477960+Rajmalbasha@users.noreply.github.com>
2020-08-31 22:24:54 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,11 @@
 #include <vector>
 
 namespace Statistics {
+    struct Stats
+    {
+        float avg=0;
+        float min=0;
+        float max=0;
+    }
     Stats ComputeStatistics(const std::vector<___>& );
 }
